161e5701d81246f91008050763be4680790b7b6f
Raúl G. González <raulggonzalezsw@outlook.com>
2015-07-13 13:18:53 +0200

    Initial commit

---
+++ dist/es5/nodejs/justo-runner/lib/index.js
@@ -0,0 +1,125 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
+
+var _justoInjector = require("justo-injector");
+
+/**
+ * Base for the errors throwing by the runner.
+ *
+ * @param item:Item     The item throwing the error.
+ * @param error:object  The error thrown.
+ */
+
+var RunError = (function (_Error) {
+  _inherits(RunError, _Error);
+
+  /**
+   * Constructor.
+   *
+   * @param(attr) item
+   * @param(attr) error
+   */
+
+  function RunError(item, error) {
+    _classCallCheck(this, RunError);
+
+    _get(Object.getPrototypeOf(RunError.prototype), "constructor", this).call(this, { message: error.toString() });
+
+    Object.defineProperty(this, "item", { value: item, enumerable: true });
+    Object.defineProperty(this, "error", { value: error, enumerable: true });
+  }
+
+  return RunError;
+})(Error);
+
+exports.RunError = RunError;
+
+/**
+ * A runner.
+ */
+
+var Runner = (function () {
+  function Runner() {
+    _classCallCheck(this, Runner);
+  }
+
+  _createClass(Runner, [{
+    key: "runSync",
+
+    /**
+     * Runs a function synchronously.
+     * Returns an object with the result: time, milliseconds; and error, if failed.
+     *
+     * @param fn:function The function to run.
+     * @param con:object  The run context.
+     * @param opts:object The run options: params (object), timeout (number).
+     *
+     * @return object
+     */
+    value: function runSync(fn, con, opts) {
+      var args, start, end, error;
+
+      //(1) run
+      if (opts.params) args = (0, _justoInjector.inject)({ params: opts.params }, fn);else args = [];
+
+      try {
+        start = Date.now();
+        fn.apply(con || fn, args);
+      } catch (e) {
+        error = e;
+      } finally {
+        end = Date.now();
+      }
+
+      //(2) return result
+      return { time: end - start, error: error };
+    }
+  }, {
+    key: "runAsync",
+
+    /**
+     * Runs a function asynchronously.
+     *
+     * @param fn:function   The function to run.
+     * @param con:object    The run context.
+     * @param opts:object   The run options: params and timeout.
+     * @param done:function The function to call: fn({time, error}).
+     */
+    value: function runAsync(fn, con, opts, _done) {
+      var args, start, end, error;
+
+      //(1) run
+      args = (0, _justoInjector.inject)({
+        params: opts.params,
+        done: function done(error) {
+          end = Date.now();
+          _done({ time: end - start, error: error });
+        }
+      }, fn);
+
+      try {
+        start = Date.now();
+        fn.apply(con || fn, args);
+      } catch (e) {
+        error = e;
+        end = Date.now();
+        _done({ time: end - start, error: error });
+      }
+    }
+  }]);
+
+  return Runner;
+})();
+
+exports.Runner = Runner;
---
+++ dist/es5/nodejs/justo-runner/test/unit/lib/Runner.js
@@ -0,0 +1,144 @@
+//imports
+const assert = require("assert");
+const Runner = require("../../../dist/es5/nodejs/justo-runner").Runner;
+
+//suite
+describe("Runner", function() {
+  var runner;
+
+  before(function() {
+    runner = new Runner();
+  });
+
+  describe("#runSync()", function() {
+    describe("Non-parameterized", function() {
+      it("runSync(fn, undefined, {}) - pass", function() {
+        function fn() { for (var i = 0; i < 1000000; ++i) i = i; }
+        var res;
+
+        res = runner.runSync(fn, undefined, {});
+        res.must.have({error: undefined});
+        res.time.must.be.instanceOf(Number);
+        res.time.must.be.gt(0);
+      });
+
+      it("runSync(fn, undefined, {}) - fail", function() {
+        function fn() {
+          for (var i = 0; i < 1000000; ++i) i = i;
+          throw new Error("The error.");
+        }
+
+        var res;
+
+        res = runner.runSync(fn, undefined, {});
+        res.time.must.be.instanceOf(Number);
+        res.time.must.be.gt(0);
+        res.error.must.be.instanceOf(Error);
+        res.error.message.must.be.eq("The error.");
+      });
+    });
+
+    describe("Parameterized", function() {
+      it("runSync(fn, undefined, {params}) - pass", function() {
+        function fn(params) {
+          if (!params) throw new Error();
+          for (var i = 0; i < 1000000; ++i) i = i;
+        }
+        var res;
+
+        res = runner.runSync(fn, undefined, {params: 123});
+        res.must.have({error: undefined});
+        res.time.must.be.instanceOf(Number);
+        res.time.must.be.gt(0);
+      });
+
+      it("runSync(fn, undefined, {params}) - fail", function() {
+        function fn(params) {
+          for (var i = 0; i < 1000000; ++i) i = i;
+          throw new Error(params);
+        }
+
+        var res;
+
+        res = runner.runSync(fn, undefined, {params: "The error."});
+        res.time.must.be.instanceOf(Number);
+        res.time.must.be.gt(0);
+        res.error.must.be.instanceOf(Error);
+        res.error.message.must.be.eq("The error.");
+      });
+    });
+  });
+
+  describe("#runAsync()", function() {
+    describe("Non-parameterized", function() {
+      it("runAsync(fn, undefined, {}, done) - pass", function(done) {
+        function fn(done) {
+          for (var i = 0; i < 1000000; ++i) i = i;
+          done();
+        }
+
+        function d(res) {
+          res.must.have({error: undefined});
+          res.time.must.be.instanceOf(Number);
+          res.time.must.be.gt(0);
+          done();
+        }
+
+        runner.runAsync(fn, undefined, {}, d);
+      });
+
+      it("runAsync(fn, undefined, {}) - fail", function(done) {
+        function fn(done) {
+          for (var i = 0; i < 1000000; ++i) i = i;
+          done(new Error("The error."));
+        }
+
+        function d(res) {
+          res.time.must.be.instanceOf(Number);
+          res.time.must.be.gt(0);
+          res.error.must.be.instanceOf(Error);
+          res.error.message.must.be.eq("The error.");
+          done();
+        }
+
+        runner.runAsync(fn, undefined, {}, d);
+      });
+    });
+
+    describe("Parameterized", function() {
+      it("runAsync(fn, undefined, {params}, done) - pass", function(done) {
+        function fn(done, params) {
+          if (!params) done(new Error());
+          for (var i = 0; i < 1000000; ++i) i = i;
+          done();
+        }
+
+        function d(res) {
+          res.must.have({error: undefined});
+          res.time.must.be.instanceOf(Number);
+          res.time.must.be.gt(0);
+          done();
+        }
+
+        runner.runAsync(fn, undefined, {params: 123}, d);
+      });
+
+      it("runAsync(fn, undefined, {params}) - fail", function(done) {
+        function fn(done, params) {
+          for (var i = 0; i < 1000000; ++i) i = i;
+          done(new Error(params));
+        }
+
+        function d(res) {
+          res.time.must.be.instanceOf(Number);
+          res.time.must.be.gt(0);
+          res.error.must.be.instanceOf(Error);
+          res.error.message.must.be.eq("The error.");
+          done();
+        }
+
+        runner.runAsync(fn, undefined, {params: "The error."}, d);
+      });
+    });
+  });
+});
